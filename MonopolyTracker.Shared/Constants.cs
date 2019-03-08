2f0547a1a9b18836f2b28c56dff3aa395225415d
brettm42 <brettm42@gmail.com>
2019-03-08 08:26:28 -0800

    migrated to Blazor 0.9

--- MonopolyTracker.Shared/Constants.cs
+++ MonopolyTracker.Shared/Constants.cs
@@ -3,6 +3,7 @@ namespace MonopolyTracker.Shared
 {
     public static class Constants
     {
-        public const string ResourcesPath = @"/Volumes/Unix/workspace/temp/MonopolyTracker/MonopolyTracker.Server/Resources";
+        //public const string ResourcesPath = @"/Volumes/Unix/workspace/temp/MonopolyTracker/MonopolyTracker.Server/Resources";
+        public const string ResourcesPath = @"E:\Repos\GitHub\MonopolyTracker\MonopolyTracker.Server\Resources";
     }
 }
